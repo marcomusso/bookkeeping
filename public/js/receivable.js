22468138ad3a9c3eaa68062f35c8c44e01dfe84a
Marco Musso <marco.musso@kmc.torino.it>
2015-01-14 09:50:26 +0100

    Include i18next. Small fixes and tooltip for receivable invoices page.

--- public/js/receivable.js
+++ public/js/receivable.js
@@ -1,13 +1,22 @@
 function initPage() {
   console.log( "initPage called" );
-  $.getJSON('/api/getreceivableinvoices.json', function(data){
-    // console.log(data);
+  // $(function () {
+  //   $('[data-toggle="tooltip"]').tooltip();
+  // });
+  $.getJSON('/api/receivableinvoices.json', function(data){
+    console.log(data);
     $('#invoices_receivable > tbody').empty();
     for (var i = 0; i < data.length; i++) {
-      $('#invoices_receivable tbody').append('<tr><td>'+data[i].invoice_id+'</td><td>'+data[i].invoice_date+'</td><td>'+data[i].workorder+'</td><td>'+data[i].total+'</td><td>'+data[i].due_date+'</td><td>'+data[i].paid_date+'</td>');
+      var invoice_date;
+      var paid_date;
+      var due_date;
+      var label='red';
+      if (data[i].invoice_date) { invoice_date=new Date(data[i].invoice_date); }
+      if (data[i].due_date) { due_date=new Date(data[i].due_date); }
+      if (data[i].paid_date !== "" ) { var x=new Date(data[i].paid_date); paid_date=x.toLocaleDateString('it-IT'); } else { paid_date='-'; }
+      $('#invoices_receivable tbody').append('<tr><td><a href="/api/getreceivableinvoicepdf/'+data[i].invoice_id+'.txt">'+data[i].invoice_id+'</a></td><td>'+invoice_date.toLocaleDateString('it-IT')+'</td><td>'+data[i].workorder+'</td><td>'+data[i].total+' €</td><td>'+data[i].vat+'</td><td>'+data[i].bank_transfer+' €</td><td>'+due_date.toLocaleDateString('it-IT')+'</td><td class="'+label+'">'+paid_date+'</td>');
     }
   });
-
 }
 
 function refreshPage() {
